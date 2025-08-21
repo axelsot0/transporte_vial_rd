63384c8bbe14d53e0b7a6a1c736516411ffec087
axelsot0 <axelmlsoto@gmail.com>
2025-08-20 22:26:28 -0400

    chore: aplicar .gitignore y limpiar archivos innecesarios

---
+++ android/app/src/main/kotlin/com/example/transporte_vial_rd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.transporte_vial_rd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\axelm\\transportefutura\\transporte_vial_rd\\transporte_vial_rd" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\flutter"
+  "PROJECT_DIR=C:\\Users\\axelm\\transportefutura\\transporte_vial_rd\\transporte_vial_rd"
+  "FLUTTER_ROOT=C:\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\axelm\\transportefutura\\transporte_vial_rd\\transporte_vial_rd\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\axelm\\transportefutura\\transporte_vial_rd\\transporte_vial_rd"
+  "FLUTTER_TARGET=C:\\Users\\axelm\\transportefutura\\transporte_vial_rd\\transporte_vial_rd\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\axelm\\transportefutura\\transporte_vial_rd\\transporte_vial_rd\\.dart_tool\\package_config.json"
+)
